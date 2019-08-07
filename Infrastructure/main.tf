b3554b948bf2801ecb2d2eb33481060870131953
Justin Davies <juda@microsoft.com>
2019-08-07 16:16:24 +0100

    Add in AAD

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,14 +1,13 @@
-
-resource "azurerm_resource_group" "test" {
-  name     = "acctestRG1"
-  location = "East US"
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-rbac-resources"
+  location = "${var.location}"
 }
 
-resource "azurerm_kubernetes_cluster" "test" {
-  name                = "acctestaks1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  dns_prefix          = "acctestagent1"
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-rbac"
+  location            = "${azurerm_resource_group.example.location}"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  dns_prefix          = "${var.prefix}-rbac"
 
   agent_pool_profile {
     name            = "default"
@@ -18,17 +17,22 @@ resource "azurerm_kubernetes_cluster" "test" {
     os_disk_size_gb = 30
   }
 
-  agent_pool_profile {
-    name            = "pool2"
-    count           = 3
-    vm_size         = "Standard_D2_v2"
-    os_type         = "Linux"
-    os_disk_size_gb = 30
-  }
-
   service_principal {
     client_id     = "${var.SP}"
     client_secret = "${var.PASS}"
+  }
+
+  role_based_access_control {
+    enabled = true
+
+    azure_active_directory {
+      # NOTE: in a Production environment these should be different values
+      # but for the purposes of this example, this should be sufficient
+      client_app_id = "${var.client_app_id}"
+      server_app_id     = "${var.server_app_id}"
+      server_app_secret = "${var.server_app_secret}"
+      tenant_id = "${var.tenant_id}"
+    }
   }
 
   tags = {
@@ -36,10 +40,3 @@ resource "azurerm_kubernetes_cluster" "test" {
   }
 }
 
-output "client_certificate" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config.0.client_certificate}"
-}
-
-output "kube_config" {
-  value = "${azurerm_kubernetes_cluster.test.kube_config_raw}"
-}
--- Infrastructure/vars.tf
+++ Infrastructure/vars.tf
@@ -4,3 +4,27 @@ variable "SP" {
 variable "PASS" {
       default = "foo"
 }
+
+variable "prefix" {
+  description = "A prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be provisioned"
+}
+
+variable "client_app_id" {
+  description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+}
+
+variable "server_app_id" {
+  description = "The Server ID for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+}
+
+variable "server_app_secret" {
+  description = "The Server Secret for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+}
+
+variable "tenant_id" {
+  description = "The Tenant to use for this Managed Kubernetes Cluster for RBAC"
+}
