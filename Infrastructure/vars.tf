289900613cfd3da1ad8ba360b89a29a9ff5163df
Justin Davies <juda@microsoft.com>
2019-08-07 16:22:43 +0100

    Fix vars

--- Infrastructure/vars.tf
+++ Infrastructure/vars.tf
@@ -7,24 +7,33 @@ variable "PASS" {
 
 variable "prefix" {
   description = "A prefix used for all resources in this example"
+  default = "test"
+
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
+  default = "eastus"
+
 }
 
 variable "client_app_id" {
   description = "The Client ID for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+  default = "foo"
+
 }
 
 variable "server_app_id" {
   description = "The Server ID for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+  default = "foo"
 }
 
 variable "server_app_secret" {
   description = "The Server Secret for the Service Principal to use for this Managed Kubernetes Cluster for RBAC"
+  default = "foo"
 }
 
 variable "tenant_id" {
   description = "The Tenant to use for this Managed Kubernetes Cluster for RBAC"
+  default = "foo"
 }
